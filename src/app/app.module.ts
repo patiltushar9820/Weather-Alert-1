3ff4cf844db79da9dce9be7fc18977497b8979bc
Tushar patil <patiltushar9820@gmail.com>
2021-10-13 13:54:10 +0530

    first change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ParticularAreaServiceService } from './core/services/particular-area-se
 import { RouterModule } from '@angular/router';
 import { SelectalertComponent } from './selectalert/selectalert.component';
 
+//import modules
 import {MatButtonModule} from '@angular/material/button';
 import { MatFormFieldModule, MatGridListModule, MatInputModule, MatProgressSpinnerModule, MatSelectModule} from '@angular/material'
 import {MatTableModule} from '@angular/material/table';
